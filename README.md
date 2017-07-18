ca510c027b869de68ec3c7520bcc9fda0d8fe5c1
TheAslera <pvictor@protonmail.com>
2017-07-18 12:08:03 -0400

    Made website

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # PierreBrossolette
 L'histoire de Pierre Brossolette, pour l'EPI "Ombre et Lumière" présenté à l'oral du Brevet par Victor Poulard.
+
 **Disponible à l'adresse:** https://theaslera.me/pierrebrossolette
 
 ___
---
+++ video/js/playpause.js
@@ -0,0 +1,21 @@
+﻿function vidplay() {
+	var video = document.getElementById("pierrebrossolettevideo");
+	var button = document.getElementById("playbtn");
+	if (video.paused) {
+		video.play();
+		button.innerHTML = '<img src="img/icons/pause.png" alt="Pause"/>';
+	} else {
+		video.pause();
+		button.innerHTML = '<img src="img/icons/play.png" alt="Play"/>';
+	}
+}
+
+function restart() {
+	var video = document.getElementById("pierrebrossolettevideo");
+	var button = document.getElementById("playbtn");
+	video.currentTime = 0;
+	if (video.paused) {
+		video.play();
+	}
+	button.innerHTML = '<img src="img/icons/pause.png" alt="Pause"/>';
+}
